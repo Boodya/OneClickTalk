8087edf9ad3d4b034c105b4890175342dba8c1b1
Boodya <ilyyha15@gmail.com>
2025-03-08 12:51:03 +0100

    Fix routing for github pages

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { v4 as uuidv4 } from 'uuid';
 
 @Component({
@@ -9,10 +9,19 @@ import { v4 as uuidv4 } from 'uuid';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      const roomId = params['roomId'];
+      if (roomId) {
+        this.router.navigate(['/rooms', roomId]);
+      }
+    });
+  }
 
   createRoom() {
     const roomId = uuidv4();
-    this.router.navigate(['/rooms', roomId]);
+    this.router.navigate([''], { queryParams: { roomId } });
   }
 }
--- src/app/pages/room-page/room-page.component.ts
+++ src/app/pages/room-page/room-page.component.ts
@@ -21,27 +21,22 @@ export class RoomPageComponent implements OnInit {
 
   async ngOnInit() {
     this.roomId = this.route.snapshot.params['roomId'];
-
-    // копируем ссылку на комнату (только создатель)
-    const roomLink = `${location.origin}/rooms/${this.roomId}`;
+    const roomLink = `${location.origin}/?roomId=${this.roomId}`;
     navigator.clipboard.writeText(roomLink);
     console.log('Ссылка на комнату скопирована:', roomLink);
 
     const stream = await this.peerService.getMediaStream();
     this.localVideo.nativeElement.srcObject = stream;
 
-    // Попробуем подключиться как хост
     this.peerService.initPeer(this.roomId).then((id) => {
       if (id === this.roomId) {
-        // Мы создатель комнаты
         console.log('Комната создана:', id);
         this.peerService.answerCall((remoteStream) => {
           this.remoteVideo.nativeElement.srcObject = remoteStream;
         });
       }
     }).catch(async () => {
-      // Если ID занят, значит это гость - создаем случайный peerId
-      await this.peerService.initPeer();  // без параметра = случайный ID
+      await this.peerService.initPeer();
       const call = this.peerService.callPeer(this.roomId, stream);
       call.on('stream', (remoteStream) => {
         this.remoteVideo.nativeElement.srcObject = remoteStream;
