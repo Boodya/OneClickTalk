4e0662059c5b00eb2478aa3f7570694d5d29dec2
Boodya <ilyyha15@gmail.com>
2025-03-10 19:38:26 +0100

    TURN server integration

--- src/app/services/peer.service.ts
+++ src/app/services/peer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import Peer, { MediaConnection } from 'peerjs';
+import Peer, { MediaConnection, PeerOptions } from 'peerjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,16 +11,40 @@ export class PeerService {
 
   initPeer(peerId?: string): Promise<string> {
     return new Promise((resolve, reject) => {
-      this.peer = peerId ? new Peer(peerId) : new Peer();
-
+      const config = {
+        config: {
+          iceServers: [
+            { urls: 'stun:stun.l.google.com:19302' },
+            {
+              urls: 'turn:eu-central.relay.metered.ca:80',
+              username: '',
+              credential: ''
+            },
+            {
+              urls: 'turn:eu-central.relay.metered.ca:443',
+              username: '',
+              credential: ''
+            },
+            {
+              urls: 'turns:eu-central.relay.metered.ca:443',
+              username: '',
+              credential: ''
+            }
+          ]
+        }
+      };
+  
+      this.peer = peerId ? new Peer(peerId, config) : new Peer(config);
+  
       this.peer.on('open', (id: string) => {
         console.log('Peer connected with id:', id);
         resolve(id);
       });
-
+  
       this.peer.on('error', (err) => reject(err));
     });
   }
+  
 
   async getMediaStream(): Promise<MediaStream> {
     if (!this.localStream) {
